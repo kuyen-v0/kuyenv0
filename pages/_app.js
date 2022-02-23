6ad3723c15f01f363e7d584032a15f4de3f5a1c9
Ashar Farooq <afarooq@mit.edu>
2022-02-23 01:06:23 -0500

    reformatting code layout and cleanup + basic navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,48 +1,106 @@
-import '../styles/globals.css'
-import Link from 'next/link'
-import Script from 'next/script';
-import Head from 'next/head';
-import { ChakraProvider } from '@chakra-ui/react'
+import "../styles/globals.css";
+import Link from "next/link";
+import Script from "next/script";
+import Head from "next/head";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-
     <ChakraProvider>
       <Head>
-      <link rel="stylesheet" href="https://unpkg.com/flowbite@1.3.3/dist/flowbite.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/flowbite@1.3.3/dist/flowbite.min.css"
+        />
       </Head>
 
+      <div>
+        <Script src="https://unpkg.com/flowbite@1.3.3/dist/flowbite.js"></Script>
 
-    <div>
-      <Script src="https://unpkg.com/flowbite@1.3.3/dist/flowbite.js"></Script>
+        <nav class="navbar navbar-expand-lg navbar-light relative flex w-full flex-wrap items-center justify-between bg-gray-900 py-3 text-gray-200 shadow-lg">
+          <div class="container-fluid flex w-full flex-wrap items-center justify-between px-6">
+            <button
+              class="navbar-toggler border-0 bg-transparent py-2 px-2.5 text-gray-200 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent1"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="bars"
+                class="w-6"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+                ></path>
+              </svg>
+            </button>
+            <div
+              class="collapse navbar-collapse flex-grow items-center"
+              id="navbarSupportedContent1"
+            >
+              <a class="pr-2 text-xl font-semibold text-white" href="/">
+                Kuyen Marketplace
+              </a>
+              <ul class="navbar-nav list-style-none mr-auto flex flex-col pl-0">
+                <li class="nav-item p-2">
+                  <a
+                    class="nav-link text-white opacity-60 hover:opacity-80 focus:opacity-80"
+                    href="/"
+                  >
+                    Gallery
+                  </a>
+                </li>
+                <li class="nav-item p-2">
+                  <a
+                    class="nav-link p-0 text-white opacity-60 hover:opacity-80 focus:opacity-80"
+                    href="create-item"
+                  >
+                    Sell Digital Asset
+                  </a>
+                </li>
+                <li class="nav-item p-2">
+                  <a
+                    class="nav-link p-0 text-white opacity-60 hover:opacity-80 focus:opacity-80"
+                    href="my-assets"
+                  >
+                    My Digital Assets
+                  </a>
+                </li>
+              </ul>
+            </div>
 
-
-    <nav className="border-b p-6">
-    <p className="text-4xl font-bold">Kuyen Marketplace</p>
-    <div className="flex mt-4">
-      <Link href="/">
-        <a className="mr-4 text-pink-500">
-          Gallery
-        </a>
-      </Link>
-      <Link href="/create-item">
-        <a className="mr-6 text-pink-500">
-          Sell Digital Asset
-        </a>
-      </Link>
-      <Link href="/my-assets">
-        <a className="mr-6 text-pink-500">
-          My Digital Assets
-        </a>
-      </Link>
-    </div>
-  </nav>
-  <Component {...pageProps} />
-  </div>
-  
-  </ChakraProvider>
-  )
-
+            <div class="relative flex items-center">
+              <div class="dropdown relative">
+                <a
+                  class="dropdown-toggle hidden-arrow flex items-center"
+                  href="/"
+                  id="dropdownMenuButton2"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                ></a>
+                <ul
+                  class="dropdown-menu absolute left-auto right-0 z-50 float-left m-0 mt-1 hidden hidden min-w-max list-none rounded-lg border-none bg-white bg-clip-padding py-2 text-left text-base shadow-lg"
+                  aria-labelledby="dropdownMenuButton2"
+                ></ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <Component {...pageProps} />
+      </div>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/my-assets.js
+++ pages/my-assets.js
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react'
-import { ethers } from 'ethers'
-import axios from 'axios'
-import Web3Modal from 'web3modal'
-import config from '../config';
-import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
-import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json';
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import axios from "axios";
+import Web3Modal from "web3modal";
+import config from "../config";
+// import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
+// import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json';
 
 export default function MyAssets() {
   const [nfts, setNfts] = useState([]);
-  const [loadingState, setLoadingState] = useState('not-loaded');
+  const [loadingState, setLoadingState] = useState("not-loaded");
   useEffect(() => {
-    loadNFTs()
+    loadNFTs();
   }, []);
   async function loadNFTs() {
     const web3Modal = new Web3Modal({
@@ -21,42 +21,52 @@ export default function MyAssets() {
     const provider = new ethers.providers.Web3Provider(connection);
     const signer = provider.getSigner();
 
-    const marketContract = new ethers.Contract(config.nftmarketaddress, Market.abi, signer)
-    const tokenContract = new ethers.Contract(config.nftaddress, NFT.abi, provider)
+    const marketContract = new ethers.Contract(
+      config.nftmarketaddress,
+      Market.abi,
+      signer
+    );
+    const tokenContract = new ethers.Contract(
+      config.nftaddress,
+      NFT.abi,
+      provider
+    );
     const data = await marketContract.fetchMyNFTs();
 
-    const items = await Promise.all(data.map(async i => {
-      const tokenUri = await tokenContract.tokenURI(i.tokenId);
-      const meta = await axios.get(tokenUri);
-      let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
-      let item = {
-        price, 
-        tokenId: i.tokenId.toNumber(),
-        seller: i.seller,
-        owner: i.owner,
-        image: meta.data.image,
-      }
-      return item
-    }));
-    setNfts(items)
-    setLoadingState('loaded')
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await tokenContract.tokenURI(i.tokenId);
+        const meta = await axios.get(tokenUri);
+        const price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        const item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+        };
+        return item;
+      })
+    );
+    setNfts(items);
+    setLoadingState("loaded");
   }
   return (
     <div className="flex justify-center">
       <div className="p-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-          {
-            nfts.map((nft, i) => (
-              <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <img src={nft.image} className="rounded" />
-                <div className="p-4 bg-black">
-                  <p className="text-2xl font-bold text-white">Price - {nft.price} MATIC</p>
-                </div>
+        <div className="grid grid-cols-1 gap-4 pt-4 sm:grid-cols-2 lg:grid-cols-4">
+          {nfts.map((nft, i) => (
+            <div key={i} className="overflow-hidden rounded-xl border shadow">
+              <img src={nft.image} className="rounded" />
+              <div className="bg-black p-4">
+                <p className="text-2xl font-bold text-white">
+                  Price - {nft.price} MATIC
+                </p>
               </div>
-            ))
-          }
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
