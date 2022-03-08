36d47b8e13602f7d81f875b4dfa83b4638ad2ee2
Ashar Farooq <afarooq@mit.edu>
2022-03-08 01:01:31 -0500

    adding more loading gif

--- components/LoadingPage.js
+++ components/LoadingPage.js
@@ -1,16 +1,21 @@
-import Head from "next/head";
-
 export default function LoadingPage() {
   return (
     <>
+      <center>
+        <span className="text-4xl font-bold">Loading...</span>
+      </center>
+      <br />
       <div className="flex items-center justify-center">
-        <span className="font-bold">Loading...</span>
         <br />
         <div
           className="spinner-border inline-block h-12 w-12 animate-spin rounded-full border-4"
           role="status"
         ></div>
       </div>
+      <br />
+      <center>
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/04de2e31234507.564a1d23645bf.gif" />
+      </center>
     </>
   );
 }
