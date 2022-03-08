757ff2706121228ff3eb6bbbc88e1090ef495002
Christian Scarlett <christianscarlett@dhcp-10-31-57-143.dyn.MIT.EDU>
2022-03-08 15:30:01 -0500

    move navbar to its own component

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import Script from "next/script";
 import Head from "next/head";
+import Navbar from "../components/Navbar";
 // import { ChakraProvider } from "@chakra-ui/react";
 // import Image from "next/image";
 
@@ -17,62 +18,8 @@ function MyApp({ Component, pageProps }) {
 
       <div>
         <Script src="https://unpkg.com/flowbite@1.3.3/dist/flowbite.js"></Script>
-
-        <nav className="navbar-expand-lg relative flex w-full flex-wrap items-center justify-between">
-          <div className="container-fluid flex w-full flex-wrap items-center justify-between px-6">
-            <div
-              className="collapse navbar-collapse flex-grow place-content-between items-center"
-              id="navbarSupportedContent1"
-            >
-              <div>
-                <a className="pr-2 text-xl font-semibold text-white" href="/">
-                  <img
-                    className="logo-fyat"
-                    src="https://pbs.twimg.com/profile_images/1469164041558007808/FRqpXQX5_400x400.jpg"
-                  ></img>
-                </a>
-              </div>
-              <div>
-                <ul className="navbar-nav list-style-none mr-auto flex flex-col pl-0">
-                  <li className="nav-item p-2">
-                    <a
-                      className="nav-link text-white"
-                      href="https://discord.gg/fyatlux"
-                    >
-                      <button className="bg-black py-4 px-12 text-2xl font-bold text-white duration-200 hover:scale-110">
-                        Join Our Discord
-                      </button>
-                    </a>
-                  </li>
-                  <li className="nav-item p-2">
-                    <a className="nav-link p-0 text-white" href="my-assets">
-                      <button className="bg-yellow-400 py-4 px-12 text-2xl font-bold text-black duration-200 hover:scale-110">
-                        My Collection
-                      </button>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-            <div className="relative flex items-center">
-              <div className="dropdown relative">
-                <a
-                  className="dropdown-toggle hidden-arrow flex items-center"
-                  href="/"
-                  id="dropdownMenuButton2"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                ></a>
-                <ul
-                  className="dropdown-menu absolute left-auto right-0 z-50 float-left m-0 mt-1 hidden hidden min-w-max list-none rounded-lg border-none bg-white bg-clip-padding py-2 text-left text-base shadow-lg"
-                  aria-labelledby="dropdownMenuButton2"
-                ></ul>
-              </div>
-            </div>
-          </div>
-        </nav>
+        
+        <Navbar />
         <Component {...pageProps} />
       </div>
     </div>
