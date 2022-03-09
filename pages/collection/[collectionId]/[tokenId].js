76224d6f9f1a92c3943bcada68db2ebebe949b48
Christian Scarlett <christianscarlett@dhcp-10-250-138-44.harvard.edu>
2022-03-09 01:24:23 -0500

    add opensea button

--- pages/collection/[collectionId]/[tokenId].js
+++ pages/collection/[collectionId]/[tokenId].js
@@ -4,6 +4,7 @@ import Head from "next/head";
 import useSWR from "swr";
 import LoadingPage from "../../../components/LoadingPage";
 import PageTemplate from "../../../components/PageTemplate";
+import OpenSeaButton from "../../../components/OpenSeaButton";
 
 const fetcher = async (url) => {
   const res = await fetch(url);
@@ -116,16 +117,7 @@ export default function TokenData() {
             </div>
 
             <div className="flex pt-5">
-              <button className="mr-2 mb-2 rounded-lg bg-red-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-                <a
-                  href={`https://opensea.io/assets/${query.collectionId}/${query.tokenId}`}
-                  target="_blank"
-                  title="View on OpenSea"
-                  className="back-green button view-opensea"
-                >
-                  VIEW ON OPENSEA
-                </a>
-              </button>
+              <OpenSeaButton link={`https://opensea.io/assets/${query.collectionId}/${query.tokenId}`} />
               <button className="mr-2 mb-2 rounded-lg bg-purple-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-purple-800 focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
                 <a
                   href={`https://etherscan.io/token/${query.collectionId}?a=${query.tokenId}`}
