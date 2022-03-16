0d44b9a11a8566e788a65e0504d0a87bf64cabf6
Christian Scarlett <christianscarlett@dhcp-10-31-114-162.dyn.MIT.EDU>
2022-03-16 13:39:46 -0400

    add callbacks to filter pills

--- components/FilterPill.js
+++ components/FilterPill.js
@@ -2,20 +2,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
-export default function FilterPill({ optionName }) {
+export default function FilterPill({ optionName, onClick=()=>{} }) {
   return (
-    <button className='flex items-center border-2 border-white text-white rounded mr-4 px-3 py-2 hover:shadow-2xl duration-300'>
+    <button onClick={onClick} className='flex items-center border-2 border-white text-white rounded mr-4 px-3 py-2 hover:shadow-2xl duration-300'>
       <p className='mr-2'>{optionName}</p>
-      <FontAwesomeIcon icon={faXmark} />
+      <FontAwesomeIcon icon={faXmark} className='hover:text-black duration-300' />
     </button>
   );
 }
 
-export function FilterPills({ currentlySelected=['asdf', 'test'] }) {
+export function FilterPills({
+  currentlySelected=['asdf', 'test'], // This should be a state variable from the parent page
+  onPillClick=(optionName)=>{} , // This should be passed in from the parent page to update the state variable appropriately
+}) {
   return (
     <div className='flex items-center pt-4'>
       {currentlySelected.map(optionName => (
-        <FilterPill optionName={optionName} />
+        <FilterPill optionName={optionName} onClick={() => onPillClick(optionName)} key={optionName} />
       ))}
     </div>
   )
