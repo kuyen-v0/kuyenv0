b80c3430a2f35ae33e9d2ca11b1379507318939d
Ashar Farooq <afarooq@mit.edu>
2022-03-21 20:08:12 -0400

    owner name

--- pages/api/collection/[collectionId]/[tokenId].js
+++ pages/api/collection/[collectionId]/[tokenId].js
@@ -19,25 +19,21 @@ export default async function tokenMetadata(req, res) {
 
     const item = docSnap.data();
 
-    // const options = { address: collection, token_id: tokenId, chain: "eth" };
-    // const tokenIdOwners = await Moralis.Web3API.token.getTokenIdOwners(options);
-    // item.owner = tokenIdOwners.result[0].owner_of;
-    // console.log(item.owner);
+    const options = { address: collection, token_id: tokenId, chain: "eth" };
+    const tokenIdOwners = await Moralis.Web3API.token.getTokenIdOwners(options);
+    item.owner = tokenIdOwners.result[0].owner_of;
+    console.log(item.owner);
 
-    // fetch("https://api.opensea.io/user/" + item.owner + "?format=json")
-    //   .then(res => res.json())
-    //   .then(responseJSON => {
-    //     if (responseJSON.username === null) {
-    //       item.owner_name = item.owner.substring(0, 8) + "...";
-    //     } else {
-    //       item.owner_name = responseJSON.username;
-    //     }
-    //     //item.owner_name = responseJSON.username;
-    //     res.status(200).json(item);
-    //   });
-
-    item.owner_name = "PLACEHOLDER";
-    res.status(200).json(item);
+    fetch("https://api.opensea.io/user/" + item.owner + "?format=json")
+      .then((res) => res.json())
+      .then((responseJSON) => {
+        if (responseJSON.username === null) {
+          item.owner_name = item.owner.substring(0, 8) + "...";
+        } else {
+          item.owner_name = responseJSON.username;
+        }
+        res.status(200).json(item);
+      });
   } catch (err) {
     res.status(500).json({ error: err });
   }
