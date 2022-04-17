c6288bf1284afce0609b66444f5f40641f26740c
Christian Scarlett <christianscarlett@dhcp-10-31-71-127.dyn.MIT.EDU>
2022-04-17 17:10:05 -0400

    filterpill thinner border

--- components/FilterPill.js
+++ components/FilterPill.js
@@ -4,7 +4,7 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 export default function FilterPill({ option, onClick=()=>{} }) {
   return (
-    <button onClick={() => onClick(option)} className='flex items-center border-2 border-white text-white rounded mr-4 px-3 py-2 hover:shadow-2xl duration-300'>
+    <button onClick={() => onClick(option)} className='flex items-center border border-white text-white rounded mr-4 px-3 py-2 hover:shadow-2xl duration-300'>
       <p className='mr-2'>{option.filterName}: {option.optionName}</p>
       <FontAwesomeIcon icon={faXmark} className='hover:text-black duration-300' />
     </button>
